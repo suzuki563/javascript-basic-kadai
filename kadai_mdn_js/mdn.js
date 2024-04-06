559712366923525d7517f7b79e8f1948826892a5
suzuki563 <135518774+suzuki563@users.noreply.github.com>
2024-04-07 02:38:04 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+const today = new Date();
+
+const formatDate = () => {
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1; 
+  const day = today.getDate();
+
+  console.log(year + '年' + month + '月' + day + '日');
+};
+
+formatDate();
