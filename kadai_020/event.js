d790199ad5b687c10b348ee2a568bdd180317257
suzuki563 <135518774+suzuki563@users.noreply.github.com>
2024-04-14 23:44:42 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const outputBtn = document.getElementById('btn');
+const displayText = document.getElementById('text');
+
+outputBtn.addEventListener('click', ()=> {
+   displayText.textContent = 'ボタンをクリックしました';
+});
+
+
+
